dc2a1b6de95d03aa02d7b619a1167b1077f93fcb
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2024-11-07 13:43:36 +0100

    Update upload_edit_reference_table.py

--- upload_edit_reference_table.py
+++ upload_edit_reference_table.py
@@ -517,7 +517,7 @@ elif st.session_state['selected-table']is not None:
             st.markdown(f"**Created:** {selected_row['created']}")
             st.markdown(f"**Updated:** {selected_row.get('lastImportDate', 'N/A')}")
             st.markdown(f"**Primary Key:** {selected_row.get('primaryKey', 'N/A')}")
-            st.markdown(f"**Table Setting:** {re.sub(r'```.*', '', re.sub(r'.*Upload setting:?\s*```\{', '{', selected_row.get('description', 'N/A')))}")
+            st.markdown(f"**Table Setting:** {re.sub(r'```.*', '', re.sub(r'.*Upload setting:?\s*```\{', '{', selected_row.get('description', '')))}")
             st.markdown(f"**Rows Count:** {selected_row['rowsCount']}")
 
     # Download table as CSV, TSV or Excel
