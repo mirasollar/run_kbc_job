ea90022c1595f8b3669387bed54f6296360efed1
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-01-23 11:19:57 +0100

    Table setting - dict to string 2

--- upload_edit_reference_table.py
+++ upload_edit_reference_table.py
@@ -527,7 +527,7 @@ elif st.session_state['selected-table']is not None:
             st.markdown(f"**Primary Key:** {selected_row.get('primaryKey', 'N/A')}")
             description = selected_row['description']
             table_setting = re.sub(r'```.*', '', re.sub(r'.*Upload setting:?\s*```\{', '{', description))
-            st.markdown(f"**Table Setting:** {', '.join(f"*{key}*: {value}" for key, value in table_setting.items())}")
+            st.markdown(f"**Table Setting:** {', '.join(f"{key}: {value}" for key, value in table_setting.items())}")
             case_insensitive_columns = [outer_key for outer_key, items in selected_row['column_metadata'].items() if any(item.get('value') == 'case sensitive' for item in items)]
             if case_insensitive_columns:
                 st.markdown(f"**Case Insensitive Columns:** {', '.join(case_insensitive_columns)}")
