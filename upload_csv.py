ce232a887ca9ef427a50f30e72c756b9a0ef58dd
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-01-29 08:41:10 +0100

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -8,11 +8,12 @@ uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 if uploaded_file is not None:
     file_content = uploaded_file.read()
+    result = from_path(uploaded_file).best()
+    encoding = result.encoding
     try:
         df = pd.read_csv(io.BytesIO(file_content), sep=None, engine='python', encoding='utf-8-sig')
     except:
-        result = from_path(io.BytesIO(file_content)).best()
-        encoding = result.encoding
+
         st.write(f"Detekované kodování: {encoding}")
         df = pd.read_csv(io.BytesIO(file_content), sep=None, engine='python', encoding=encoding)
         
