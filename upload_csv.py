71c6e420ed05b61694eb932628f69e0e6b226648
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-02-04 11:09:19 +0100

    Update upload_csv.py

--- upload_csv.py
+++ upload_csv.py
@@ -3,6 +3,16 @@ from charset_normalizer import from_bytes
 import streamlit as st
 import pandas as pd
 
+token = st.secrets["kbc_storage_token"]
+kbc_url = url = st.secrets["kbc_url"]
+kbc_token = st.secrets["kbc_token"]
+streamlit_protected_save = st.secrets["streamlit_protected_save"]
+
+st.write(f"KBC token: {token}")
+st.write(f"KBC url: {kbc_url}")
+st.write(f"KBC token: {kbc_token}")
+st.write(f"Streamlit protected save: {streamlit_protected_save}")
+
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 if uploaded_file is not None:
