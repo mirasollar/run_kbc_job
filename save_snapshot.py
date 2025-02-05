a721a1e33b824d5e008cfab9d3643e1195fe6d0d
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-02-05 13:57:48 +0100

    Update save_snapshot.py

--- save_snapshot.py
+++ save_snapshot.py
@@ -4,10 +4,6 @@ import streamlit as st
 import pandas as pd
 
 streamlit_protected_save = st.secrets["streamlit_protected_save"]
-
-st.write(f"Token: {token}")
-st.write(f"KBC url: {kbc_url}")
-st.write(f"KBC token: {kbc_token}")
 st.write(f"Streamlit protected save: {streamlit_protected_save}")
 
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
