980475ec47b5a94c4a545266508b1ecb94f11cc6
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-02-05 14:04:33 +0100

    Update save_snapshot.py

--- save_snapshot.py
+++ save_snapshot.py
@@ -1,9 +1,18 @@
-import io
-from charset_normalizer import from_bytes
 import streamlit as st
 import pandas as pd
+import io
+import os
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+headers = _get_websocket_headers()
+st.write(headers["Host"])
 
 streamlit_protected_save = st.secrets["streamlit_protected_save"]
 st.write(f"Streamlit protected save: {streamlit_protected_save}")
+
+
+
+headers = _get_websocket_headers()
+st.write(headers["Host"])
 
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
