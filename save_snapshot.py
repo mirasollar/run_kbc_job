edf186031b5d79ee6d3316ec49808b4f311bdaa3
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-02-06 09:53:58 +0100

    Update save_snapshot.py

--- save_snapshot.py
+++ save_snapshot.py
@@ -19,6 +19,29 @@ kbc_url = url = st.secrets["kbc_url"]
 kbc_token = st.secrets["kbc_token"]
 kbc_client = Client(kbc_url, kbc_token)
 
+def get_dataframe(table_name):
+    table_detail = kbc_client.tables.detail(table_name)
+
+    kbc_client.tables.export_to_file(table_id = table_name, path_name='')
+    list = kbc_client.tables.list()
+    
+    with open('./' + table_detail['name'], mode='rt', encoding='utf-8') as in_file:
+        lazy_lines = (line.replace('\0', '') for line in in_file)
+        reader = csv.reader(lazy_lines, lineterminator='\n')
+    if os.path.exists('data.csv'):
+        os.remove('data.csv')
+    else:
+        print("The file does not exist")
+    
+    os.rename(table_detail['name'], 'data.csv')
+    df = pd.read_csv('data.csv')
+    df = cast_columns(df)
+    return df
+
+
+st.session_state['passwords'] = 'in.c-reference_tables_metadata.passwords_mso_dev_839334747'
+st.session_state['passwords_data'] = get_dataframe(st.session_state['passwords'])
+st.write(st.session_state['passwords_data'])
 
 now_utc = datetime.now(dttimezone.utc)
 str_now_utc = now_utc.strftime('%Y-%m-%d, %H:%M:%S')
