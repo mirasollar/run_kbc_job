ce9c0170918f3ec31e3623d31445c27042b32f72
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-02-11 09:17:15 +0100

    Update save_snapshot.py

--- save_snapshot.py
+++ save_snapshot.py
@@ -47,8 +47,8 @@ st.write(f"Dataframe s daty: {df}")
 if "user_name" not in st.session_state:
     st.session_state['user_name'] = None
     
-# password_input = st.text_input("Enter password:", type="password")
-password_input = ''
+password_input = st.text_input("Enter password:", type="password")
+
 if st.button("Submit"):
     if "passwords" not in st.session_state:
         st.session_state['passwords'] = get_password_dataframe(f"in.c-reference_tables_metadata.passwords_{get_table_name_suffix()}")
