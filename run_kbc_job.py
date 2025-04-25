aa39039afc1a642047c86d228a55d815a223808a
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2025-04-25 15:08:57 +0200

    Update run_kbc_job.py

--- run_kbc_job.py
+++ run_kbc_job.py
@@ -14,6 +14,7 @@ import json
 import numpy as np
 import io
 from charset_normalizer import from_bytes
+import requests
 
 # Setting page config
 st.set_page_config(page_title="Keboola Data Editor", page_icon=":robot:", layout="wide")
